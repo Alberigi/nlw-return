5bd50a1ccd0771569aed7fe5e35969261b26121e
alberigi <mateusalberigi@gmail.com>
2022-05-29 14:38:32 -0300

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,9 @@ import { routes } from './routes'
 import cors from 'cors'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: process.env.WEB_URL,
+}))
 app.use(express.json())
 app.use(routes)
 
